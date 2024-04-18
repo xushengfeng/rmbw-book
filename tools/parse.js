be1ed3ec04585e7d7e8aa38ac2fb2a0e3e26c4ab
xushengfeng <xushengfeng_zg@163.com>
2024-04-18 18:21:10 +0800

    工具 格式化更新

--- tools/parse.js
+++ tools/parse.js
@@ -4,6 +4,8 @@ const index = getIndex();
 
 const booksId = process.argv.slice(2);
 
+const englishMap = { "，": ",", "。": ".", "？": "?", "！": "!", "：": ":", "；": ";" };
+
 for (let b of index.books) {
     if (b.type === "text") {
         let matchBook = false;
@@ -11,8 +13,12 @@ for (let b of index.books) {
         for (let s of b.sections) {
             if (matchBook || booksId.includes(s.id)) {
                 let text = getFile(s.path);
-                text = text.replace(/([,.!?])([A-Za-z])/g, "$1 $2");
+                for (let i in englishMap) {
+                    text = text.replaceAll(i, englishMap[i]);
+                }
+                text = text.replace(/([,.!?;])([A-Za-z])/g, "$1 $2");
                 text = text.replace(/ +/g, " ");
+                text = text.replace(/(\w+)'(\w+)/g, "$1’$2");
                 writeFile(s.path, text);
             }
         }

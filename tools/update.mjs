6594912b00374ac0a1b6fc562559b8606bd2ac26
xushengfeng <xushengfeng_zg@163.com>
2024-05-02 18:30:18 +0800

    书 更新时间

--- tools/update.mjs
+++ tools/update.mjs
@@ -2,13 +2,21 @@ import { getIndex, writeIndex } from "./until.mjs";
 const index = getIndex();
 import { execSync } from "child_process";
 
-execSync("git config core.quotepath false");
-const r = execSync("git status -s source").toString();
-const rl = r.split("\n").map((i) => i.slice(10));
+const booksId = process.argv.slice(2);
 
-for (let b of index.books) {
-    for (let s of b.sections) {
-        if (rl.includes(s.path)) b.updateTime = new Date().getTime();
+if (booksId.length) {
+    for (const b of index.books) {
+        if (booksId.includes(b.id)) b.updateTime = new Date().getTime();
+    }
+} else {
+    execSync("git config core.quotepath false");
+    const r = execSync("git status -s source").toString();
+    const rl = r.split("\n").map((i) => i.slice(10));
+
+    for (let b of index.books) {
+        for (let s of b.sections) {
+            if (rl.includes(s.path)) b.updateTime = new Date().getTime();
+        }
     }
 }
 

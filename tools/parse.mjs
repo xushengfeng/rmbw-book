b355ae3e4be545a6c98242c1b082d5a8c350e01a
xushengfeng <xushengfeng_zg@163.com>
2024-05-04 22:40:03 +0800

    人教版初中七年级上

--- tools/parse.mjs
+++ tools/parse.mjs
@@ -5,6 +5,7 @@ const index = getIndex();
 const booksId = process.argv.slice(2);
 
 const englishMap = { "，": ",", "。": ".", "？": "?", "！": "!", "：": ":", "；": ";" };
+const chineseMap = { ",": "，", ".": "。", "?": "？", "!": "！", ":": "：", ";": "；" };
 
 for (let b of index.books) {
     let matchBook = false;
@@ -12,20 +13,34 @@ for (let b of index.books) {
     for (let s of b.sections) {
         if (matchBook || booksId.includes(s.id)) {
             let text = getFile(s.path);
-            if (b.type === "text") {
-                for (let i in englishMap) {
-                    text = text.replaceAll(i, englishMap[i]);
+            if (b.language === "en") {
+                if (b.type === "text") {
+                    for (let i in englishMap) {
+                        text = text.replaceAll(i, englishMap[i]);
+                    }
+                    text = text.replace(/([,.!?;])([A-Za-z])/g, "$1 $2");
+                    text = text.replace(/ +/g, " ");
+                    text = text.replace(/(\w+)'(\w+)/g, "$1’$2");
+                    writeFile(s.path, text);
                 }
-                text = text.replace(/([,.!?;])([A-Za-z])/g, "$1 $2");
-                text = text.replace(/ +/g, " ");
-                text = text.replace(/(\w+)'(\w+)/g, "$1’$2");
-                writeFile(s.path, text);
+                if (b.type === "word") {
+                    let l = text.split("\n");
+                    l = l.map((w) => w.trim());
+                    l = Array.from(new Set(l));
+                    writeFile(s.path, l.join("\n"));
+                }
             }
-            if (b.type === "word") {
-                let l = text.split("\n");
-                l = l.map((w) => w.trim());
-                l = Array.from(new Set(l));
-                writeFile(s.path, l.join("\n"));
+            if (b.language === "cn") {
+                if (b.type === "text") {
+                    text = text.replace(/(\d)/g, "");
+                    text = text.replace(/\n+/g, "\n");
+                    text = text.replace(/ +/g, "");
+                    for (let i in chineseMap) {
+                        text = text.replaceAll(i, chineseMap[i]);
+                    }
+                    text = text.trim();
+                    writeFile(s.path, text);
+                }
             }
         }
     }
